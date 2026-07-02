8d33e7ab74af492f923aa852ebc12726dfe4b8b5
Bastian Ibáñez <ibanezbastian34@gmail.com>
2026-07-01 20:35:03 -0400

    Preparado para el despliegue

--- app/config.py
+++ app/config.py
@@ -1,12 +1,9 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 class Config:
-    DB_HOST = os.getenv("DB_HOST")
-    DB_PORT = os.getenv("DB_PORT")
-    DB_NAME = os.getenv("DB_NAME")
-    DB_USER = os.getenv("DB_USER")
-    DB_PASSWORD = os.getenv("DB_PASSWORD")
-    SECRET_KEY = os.getenv("SECRET_KEY")
+    DB_HOST = os.environ.get("DB_HOST")
+    DB_PORT = os.environ.get("DB_PORT")
+    DB_NAME = os.environ.get("DB_NAME")
+    DB_USER = os.environ.get("DB_USER")
+    DB_PASSWORD = os.environ.get("DB_PASSWORD")
+    SECRET_KEY = os.environ.get("SECRET_KEY")
--- app/database/connection.py
+++ app/database/connection.py
@@ -3,9 +3,9 @@ from app.config import Config
 
 def get_connection():
     return psycopg2.connect(
-        host=str(Config.DB_HOST),
-        port=int(Config.DB_PORT),
-        database=str(Config.DB_NAME),
-        user=str(Config.DB_USER),
-        password=str(Config.DB_PASSWORD)
+        host=Config.DB_HOST,
+        port=int(Config.DB_PORT or 5432),
+        database=Config.DB_NAME,
+        user=Config.DB_USER,
+        password=Config.DB_PASSWORD
     )
